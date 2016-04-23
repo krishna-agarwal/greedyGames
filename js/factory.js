60487d3c77968d0e33c7d53ad85c14991927af9d
krishna Agrawal <krishitnitp@gmail.com>
2016-04-23 16:00:17 +0530

    protocol changed to http

--- js/factory.js
+++ js/factory.js
@@ -1,7 +1,7 @@
 angular.module('factory',[])
 .factory('RestService',function($resource){
 	return{
-		track : $resource('https://104.197.128.152:8000/v1/tracks/:id',{id: '@id'}),
-		genre : $resource('https://104.197.128.152:8000/v1/genres/:id',{id: '@id'})
+		track : $resource('http://104.197.128.152:8000/v1/tracks/:id',{id: '@id'}),
+		genre : $resource('http://104.197.128.152:8000/v1/genres/:id',{id: '@id'})
 	} 
 })
